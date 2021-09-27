658acbb18a50d2f6233dea662843dab2cc28011c
Sanm-ZH <zhxpvip7@gmail.com>
2021-09-27 19:52:14 +0800

    Update 注入element plus

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
+import 'element-plus/dist/index.css'
+import '@/styles/common.scss'
+import { ElButton, ElDialog, ElCard } from 'element-plus'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component(ElButton.name, ElButton)
+app.component(ElDialog.name, ElDialog)
+app.component(ElCard.name, ElCard)
+
+app.mount('#app')
