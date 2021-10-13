dc60b95ff2a5da37184dd3695f4c3a58a879a72e
Sanm-ZH <zhxpvip7@gmail.com>
2021-10-13 11:00:01 +0800

    Update gh-pages 目录问题

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? './'
+    : '/',
   configureWebpack: {
     plugins: [
       require('unplugin-vue-components/webpack')({ /* options */ })
